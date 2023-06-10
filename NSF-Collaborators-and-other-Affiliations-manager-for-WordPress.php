d0761e104c15bb5d95a183e95c7bcd6a28ebf801
Bryan Hoffman <bryanhoffman1@gmail.com>
2023-06-09 23:16:29 -0500

    new branch as plugin

---
+++ NSF-Collaborators-and-other-Affiliations-manager-for-WordPress.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+ * Plugin Name:       NSF COA Manager for WordPress
+ * Description:       Manage publications and helps prepare NSF's COA form
+ * Author:            Bryan Hoffman
+ * Author URI:        https://bryanhoffman.xyz/
+ */
